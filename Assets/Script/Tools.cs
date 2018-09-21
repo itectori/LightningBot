98023ec421e583bef795cc89917e106994f8fd41
Corentin <corentin.mounier@epita.fr>
2018-09-21 23:06:03 +0200

    Added Tools class + Dezoom camera + Change .Net version

---
+++ Assets/Script/Tools.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections;
+using UnityEngine;
+using UnityEngine.Networking;
+
+namespace Script
+{
+    public static class Tools
+    {
+        public static IEnumerator WebRequest(string url, Action<string> callback)
+        {
+            using (var www = UnityWebRequest.Get(url))
+            {
+                yield return www.SendWebRequest();
+
+                if (www.isNetworkError || www.isHttpError)
+                    Debug.Log(www.error);
+                else
+                    callback(www.downloadHandler.text);
+            }
+        }
+    }
+}
