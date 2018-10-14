8752124761be7061e974f9b25a0c0a3a1c45d98e
Corentin <corentin.mounier@epita.fr>
2018-10-14 17:47:28 +0200

    Display current version

---
+++ Assets/Script/Version.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using Script;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Version : MonoBehaviour
+{
+	private Text version;
+	
+	void Start ()
+	{
+		print("start");
+		version = GetComponent<Text>();
+		StartCoroutine(Tools.WebRequest("lightningbot.tk/version", DisplayVersion));
+	}
+
+	private void DisplayVersion(string s)
+	{
+		version.text = "< " + s + " >";
+	}
+}
